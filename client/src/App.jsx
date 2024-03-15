b25dd7f44268edc2906e269605c734782947b796
wachaa <bishantkayastha01@gmail.com>
2024-03-15 21:17:29 +0545

    Start

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from './pages/Home'
+import Layout from "./Layout"
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+  
+    <Routes>
+{/* register and log in here  */}
+      <Route path="/" element={<Layout/>} > 
+{/* Profile page goes here */}
+      <Route path="/" element={<Home/>}/>
+
+      </Route>
+
+    </Routes>
+
+    </BrowserRouter>
   )
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,7 @@
 const Home = () => {
   return (
     <div>Home
+      
       <p>Hello</p>
     </div>
   )
