c0040d7ddeb48c9c6ca3ee5ae28c808a7e5eda70
Pranish Bajracharya <pranish2017bajracharya@gmail.com>
2024-03-27 12:24:09 +0545

    crud update

--- server/routers/posts.route.js
+++ server/routers/posts.route.js
@@ -1,12 +1,12 @@
-const { createUserPost, readUserPost, readFeedPost,  updatePost, deletePosts } = require('../controllers/post.controller');
+const { createUserPost, readUserPost, readFeedPost,  updateUserPost, deletePosts } = require('../controllers/post.controller');
 
 const router = require('express').Router();
 const upload = require('../utils/fileUpload');
 
 router.post('/create-post', upload.single('image'), createUserPost);
-router.get('/read-post', readFeedPost);
-router.get('/read-user-post/:id', readUserPost);
-router.put('/update-post/:updatetId', updatePost);
+router.get('/get-feed-post', readFeedPost);
+router.get('/get-user-post/:id', readUserPost);
+router.put('/update-post/:id', upload.single('image'), updateUserPost);
 router.delete('/delete-post/:deleteId', deletePosts);
 
 module.exports = router;
