3bd31efe607c37a4ae6ca6d17801db256dfab538
Sadhan-Shakya <sadhanshakya83@gmail.com>
2024-03-30 16:57:53 +0545

    like and dislike

--- server/routers/user.route.js
+++ server/routers/user.route.js
@@ -7,9 +7,6 @@ const router = require('express').Router();
 router.get('/user-profile/:id', getUserDetails);
 router.put('/user-profile/edit/:id', authMiddleware, updateUserDetails);
 router.delete('/user-profile/delete/:id', authMiddleware, deleteUserDetails);
-
-
-
 router.put('/follow/:id',authMiddleware,followUser)
 router.put('/unfollow/:id',authMiddleware,unfollowUser)
 
